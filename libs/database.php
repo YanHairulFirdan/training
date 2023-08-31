1b12d60bd842da40f6ea4510fe180b5c95177e41
YanHairulFirdan <56463087+YanHairulFirdan@users.noreply.github.com>
2023-08-31 08:34:31 +0800

    using prepared statement

--- libs/database.php
+++ libs/database.php
@@ -14,12 +14,19 @@ if (!$connection) {
 
 function insert_data($connection, $table, $data)
 {
-  $columns = implode_column_names(array_keys($data));
-  $values  = implode_values($data);
+  $colums = implode_column_names(array_keys($data));
+  
+  $bindingStatement = make_binding_statement($data);
+  
+  $valuesType = get_values_type_binding($data);
+  
+  $query  = "INSERT INTO `{$table}` ({$colums}) VALUES ({$bindingStatement})";
 
-  $query = "INSERT INTO {$table} ($columns) VALUES ($values)";
+  $preparedStatement = mysqli_prepare($connection, $query);
 
-  $result = mysqli_query($connection, $query);
+  mysqli_stmt_bind_param($preparedStatement, $valuesType, ...array_values($data));
+
+  $result = mysqli_stmt_execute($preparedStatement);
 
   return $result;
 }
@@ -33,11 +40,18 @@ function implode_column_names($columns)
   return implode(",", $columns);
 }
 
-function implode_values($values)
-{
-  $values = array_map(function ($value) {
-    return "'{$value}'";
+function get_values_type_binding($values) {
+  $valuesType = array_map(function ($value) {
+    return gettype($value)[0];
   }, $values);
 
-  return implode(",", $values);
+  return implode("", $valuesType);
+}
+
+function make_binding_statement(array $data) {
+  $bindingStatement = array_map(function ($value) {
+    return "?";
+  }, $data);
+
+  return implode(",", $bindingStatement);
 }
