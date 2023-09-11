d034657f98a5822894d11eedc9310c48a703833b
YanHairulFirdan <56463087+YanHairulFirdan@users.noreply.github.com>
2023-09-11 08:18:18 +0800

    update docs comment

--- libs/pagination.php
+++ libs/pagination.php
@@ -9,10 +9,33 @@ function make_pagination_numbering($perPage, $page, $index)
   }
 
   // jika di halaman kedua keatas
-  // ((2 - 1) * 10) + (0 + 1)
+  // misal index ke 0, maka akan menjadi 11
+  // ((2 - 1) * 10) + (0 + 1) = 11
   return (($page - 1) * $perPage) + ($index + 1);
 }
 
+/**
+ * fungsi ini akan menghasilkan pagination seperti berikut
+ * <nav aria-label="Page navigation example">
+ *   <ul class="pagination justify-content-center">
+ *    <li class="page-item">
+ *      <a class="page-link" href="?page=1" aria-label="Previous">
+ *        <span aria-hidden="true">&laquo;</span>
+ *      </a>
+ *    </li>
+ *    <li class="page-item"><a class="page-link" href="?page=1">1</a></li>
+ *    <li class="page-item"><a class="page-link" href="?page=2">2</a></li>
+ *    <li class="page-item"><a class="page-link" href="?page=3">3</a></li>
+ *    <li class="page-item"><a class="page-link" href="?page=4">4</a></li>
+ *    <li class="page-item"><a class="page-link" href="?page=5">5</a></li>
+ *    <li class="page-item">
+ *      <a class="page-link" href="?page=2" aria-label="Next">
+ *        <span aria-hidden="true">&raquo;</span>
+ *      </a>
+ *    </li>
+ *   </ul>
+ * </nav>
+ */
 function build_pagination($totalData, $perPage, $currentPage)
 {
   $totalPage = ceil($totalData / $perPage);
